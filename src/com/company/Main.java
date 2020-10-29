705346c22e065c8f223ac945d4702f1bcce23d3d
Hyeeun Lee <21545@student.dorset-college.ie>
2020-10-29 13:40:06 +0000

    first

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,9 +3,26 @@ package com.company;
 public class Main
 {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
+    }
+
+    public static void driverTest(){
+        testPerson();
+        testShop();
+        testCar();
+    }
+
+    public static void testPerson(){
+        Person P = new Person();
+    }
+
+    public static void testCar(){
+        Car c = new Car();
+    }
+
+    public static void testShop(){
+        Shop s = new Shop();
     }
 
 }
